0db0ba7f8e2beff9494db71a62679568d1d2efb9
Pardis Pashakhanloo <ppashakhanloo@gmail.com>
2016-12-08 14:17:16 +0330

    Make compatible with django 10

--- Phi/Phi/urls.py
+++ Phi/Phi/urls.py
@@ -4,30 +4,33 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from services import views as services_views
+from users import views as users_views
+
 
 urlpatterns = [
-    url(r'^$', 'services.views.home'),
-    url(r'^FAQ/$', 'services.views.answer'),
-    url(r'^login/$', 'users.views.our_login'),
-    url(r'^register/$', 'users.views.register'),
+    url(r'^$', services_views.home),
+    url(r'^FAQ/$', services_views.answer),
+    url(r'^login/$', users_views.our_login),
+    url(r'^register/$', users_views.register),
     url(r'^members/(?P<username>[\w|\d|_|@|+|.|-]+)/', include([
-    	url(r'^$', 'services.views.get_user_profile'),
-		url(r'^edit/$', 'services.views.edit_user_profile'),
-		url(r'^follow_unfollow/$', 'services.views.follow_unfollow'),
-		url(r'^following/$', 'services.views.get_followees'),
-		url(r'^followers/$', 'services.views.get_followers'),
+    	url(r'^$', services_views.get_user_profile),
+		url(r'^edit/$', services_views.edit_user_profile),
+		url(r'^follow_unfollow/$', services_views.follow_unfollow),
+		url(r'^following/$', services_views.get_followees),
+		url(r'^followers/$', services_views.get_followers),
 	])),
 	url(r'^posts/(?P<post_id>[\d]+)/', include([
-			url(r'^$', 'services.views.get_single_post'),
-			url(r'^like_unlike/$', 'services.views.like_unlike'),
-			url(r'^comment/$', 'services.views.comment'),
+			url(r'^$', services_views.get_single_post),
+			url(r'^like_unlike/$', services_views.like_unlike),
+			url(r'^comment/$', services_views.comment),
 	])),
     url(r'^movies/(?P<movie_id>[\d]+)/', include([
-    	url(r'^$', 'services.views.get_movie_profile'),
-    	url(r'^rate_post/$', 'services.views.rate_post'),
+    	url(r'^$', services_views.get_movie_profile),
+    	url(r'^rate_post/$', services_views.rate_post),
     ])),
-    url(r'^search/$', 'services.views.search'),
-    url(r'^logout/$', 'users.views.our_logout'),
+    url(r'^search/$', services_views.search),
+    url(r'^logout/$', users_views.our_logout),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^captcha/', include(captcha.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
