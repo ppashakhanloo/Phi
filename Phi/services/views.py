06734d2e82eb17725998a80f63506ba0894d278c
Pardis Pashakhanloo <ppashakhanloo@gmail.com>
2016-12-09 20:28:25 +0330

    Update views.py

--- Phi/services/views.py
+++ Phi/services/views.py
@@ -408,13 +408,13 @@ def rate_post(request, movie_id):
 		if request.method == 'POST':
 			form = SendPostModelForm(request.POST)
 			if form.is_valid():
+				movie.total_raters = movie.total_raters + 1
+				movie.avg_rate = round((movie.avg_rate * movie.total_raters + rate) / (movie.total_raters + 1), 1)
 				if form.cleaned_data['post_text'] != '':
 					post = form.save(commit=False)
 					rate = int(request.POST['rate'])
 					post.rate = rate
 					movie = models.Movie.objects.get(id=int(movie_id))
-					movie.avg_rate = round((movie.avg_rate * movie.total_raters + rate) / (movie.total_raters + 1), 1)
-					movie.total_raters = movie.total_raters + 1
 					movie.save()
 					post.datetime = datetime.datetime.now()
 					post.member = member
@@ -424,8 +424,6 @@ def rate_post(request, movie_id):
 				else:
 					rate = int(request.POST['rate'])
 					movie = models.Movie.objects.get(id=int(movie_id))
-					movie.avg_rate = round((movie.avg_rate * movie.total_raters + rate) / (movie.total_raters + 1), 1)
-					movie.total_raters = movie.total_raters + 1
 					movie.save()
 					return HttpResponseRedirect('/movies/' + movie_id + '/')
 			return HttpResponseRedirect('/movies/' + movie_id + '/')
